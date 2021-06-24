44628496804eb5664d1da91a4f8aada697603d06
akmhmgc <akimuhamaguchi@icloud.com>
2021-06-24 17:56:28 +0900

    #6 Add 順位を取得するタスクを追加

--- app/models/search_info.rb
+++ app/models/search_info.rb
@@ -4,6 +4,6 @@ class SearchInfo < ApplicationRecord
   # 登録されているキーワード、商品URLから。ランキングを取得
   def ranking
     item_top30_url_array = RakutenWebService::Ichiba::Item.search(keyword: keyword).map { |n| n["itemUrl"] }
-    item_top30_url_array.index { |s| s =~ /#{url}/ }
+    item_top30_url_array.index { |s| s =~ /#{url}/ } || "30位以下または商品URLが間違っています。"
   end
 end

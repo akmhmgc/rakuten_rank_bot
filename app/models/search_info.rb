19768555b37424ee0b40c2f203cb2febcc0e08c6
akmhmgc <akimuhamaguchi@icloud.com>
2021-06-24 12:53:56 +0900

    #5 Add 楽天APIを使用して現在のランキングを取得

--- app/models/search_info.rb
+++ app/models/search_info.rb
@@ -1,3 +1,9 @@
 class SearchInfo < ApplicationRecord
   belongs_to :user
+
+  # 登録されているキーワード、商品URLから。ランキングを取得
+  def ranking
+    item_top30_url_array = RakutenWebService::Ichiba::Item.search(keyword: keyword).map { |n| n["itemUrl"] }
+    item_top30_url_array.index { |s| s =~ /#{url}/ }
+  end
 end

39f8974e5b4858f08fe76e0f745e56d01a9e23bb
akmhmgc <akimuhamaguchi@icloud.com>
2021-06-25 11:45:39 +0900

    #7 Add ランキングの保存機能の追加

--- app/models/search_info.rb
+++ app/models/search_info.rb
@@ -1,8 +1,9 @@
 class SearchInfo < ApplicationRecord
   belongs_to :user
+  has_many :ranks
 
   # 登録されているキーワード、商品URLから。ランキングを取得
-  def ranking
+  def check_ranking
     item_top30_url_array = RakutenWebService::Ichiba::Item.search(keyword: keyword).map { |n| n["itemUrl"] }
     item_top30_url_array.index { |s| s =~ /#{url}/ } || "30位以下または商品URLが間違っています。"
   end
